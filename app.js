8520cabbcbeaf0c5b23c94590e00440859d7c799
Pedro Serodio <serodiomg@gmail.com>
2022-03-07 22:10:38 -0300

    Alterações

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { Client, Intents, MessageEmbed } = require('discord.js');
 const { token } = require('./config.json');
+var countSentiu = 0
 
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
@@ -12,23 +13,25 @@ client.once('ready', () =>{
         }],
         status: "online"
     })
-    console.log(`Logado em ${client.guilds.cache.size} servers`)
     console.log("Bot on")
 });
 
+
 client.on('messageCreate', message => {
 
-    let countSentiu = 0
-
-
+    if (message.content.toUpperCase() == "277353"){
+        message.channel.send(`Logado em ${client.guilds.cache.size} servers`)
+        message.channel.send(`Foram ${countSentiu} execuções`)
+    }
+    
+    
+    
     if (message.content.toUpperCase() == 'GALVÃO?') {
-
+        
         message.channel.send("Diga lá, Tino!");
-
+        
         countSentiu++
-
-        console.log(`Ja foi ${countSentiu} SENTIUUUU`);
-
+        
     }
 })
 
